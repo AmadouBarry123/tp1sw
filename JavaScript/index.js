cec7080b13fa52acdd998c35fd39f5d20fbad358
AmadouBarry123 <120675402+AmadouBarry123@users.noreply.github.com>
2024-03-02 20:41:38 +0000

    petites modification Css .Ajout de la fonctionnalite qui fait en sorte que si lutilisateur se cree un compte cest informations sont sauvegardes

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,9 +1,48 @@
 ////////////////// Obtenez le modal d'inscription ///////////////////////
-function userInfo (){
 
-  // Todo
-  document.location='accueil.html';
+
+function registerUser() {
+  
+  var email = document.getElementsByName('email')[0].value;
+  var username = document.getElementsByName('uname')[0].value;
+  var password = document.getElementsByName('psw')[0].value;
+  
+  //Stocker valeurs
+
+  localStorage.setItem('email', email);
+  localStorage.setItem('username', username);
+  localStorage.setItem('password', password);
+
 }
+
+
+function userInfo() {
+  // Associer les variables au valeur entree dans linscription
+  var username = document.getElementsByName('uname')[1].value;
+  var password = document.getElementsByName('psw')[1].value;
+  
+  var storedUsername = localStorage.getItem('username');
+  var storedPassword = localStorage.getItem('password');
+  // Comparer les valeurs pour voir si sa fonctionne
+  if(username === storedUsername && password === storedPassword) {
+      alert('Connexion réussie');
+      window.location.href = 'accueil.html';
+   
+  } else {
+      alert('Les informations de connexion ne correspondent pas');
+     
+  }
+}
+
+ // Relier 
+document.querySelector('.signupbtn').addEventListener('click', function(event) {
+  registerUser();
+});
+
+document.getElementById('sub').addEventListener('click', function(event) {
+  userInfo();
+});
+
 
 
 
